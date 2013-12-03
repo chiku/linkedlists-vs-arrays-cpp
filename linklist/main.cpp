d3fd0401dbe525ae7caeec806326c74463ccfc14
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-03 11:50:41 +0530

    Repeat insert times for arrays and linked-lists

--- linklist/main.cpp
+++ linklist/main.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "linklist.h"
+
 int main()
 {
-	return 0;
+	const int MAX = 10000;
+	const int REPEATS = 100;
+	double sumOfTimes;
+
+	for (int insertions = 0; insertions <= MAX; insertions += 100) {
+		sumOfTimes = 0.0;
+
+		for (int iterations = 0; iterations < REPEATS; iterations++) {
+			Linklist list;
+			std::clock_t start = std::clock();
+
+			for (int i = 0; i < insertions; i++) {
+				list.insert(rand(), rand() % MAX);
+			}
+
+			std::clock_t end = std::clock();
+			sumOfTimes += (1000.0 * (end - start) / CLOCKS_PER_SEC);
+		}
+		std::cout << insertions
+		          << ","
+		          << (sumOfTimes / REPEATS)
+		          << std::endl;
+	}
 }
