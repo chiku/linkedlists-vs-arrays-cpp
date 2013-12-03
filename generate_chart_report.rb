a95393706282abcdd773bffea94909fb4e1aabed
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-03 11:52:21 +0530

    Ruby scripts to generate chart and speadsheet reports from CSV results

---
+++ generate_chart_report.rb
@@ -0,0 +1,23 @@
+require "csv"
+require "erb"
+
+sizes = []
+arrays = []
+linked_lists = []
+
+CSV.foreach("outputs/arraylist.csv") do |(sample_size, time)|
+  sizes << sample_size.to_f
+  arrays <<  time.to_f
+end
+
+CSV.foreach("outputs/linklist.csv") do |(sample_size, time)|
+  linked_lists << time.to_f
+end
+
+container_sizes = sizes.join(", ")
+array_durations = arrays.join(", ")
+linked_list_durations = linked_lists.join(", ")
+b = binding
+result = ERB.new(File.read("public/comparison.js.erb")).result b
+
+File.open("public/comparison.js", "w") { |f| f << result }
---
+++ generate_spreadsheet_report.rb
@@ -0,0 +1,34 @@
+require "csv"
+require "axlsx"
+
+sample_to_times = {}
+
+CSV.foreach("outputs/arraylist.csv") do |(sample_size, time)|
+  sample_to_times[sample_size.to_f] = [time.to_f]
+end
+
+CSV.foreach("outputs/linklist.csv") do |(sample_size, time)|
+  sample_to_times[sample_size.to_f] << time.to_f
+end
+
+size = sample_to_times.size
+
+Axlsx::Package.new(:author => "Chirantan") do |p|
+  p.workbook.add_worksheet(:name => "Arrays vs Link-list") do |sheet|    
+    sheet.add_row ["List size", "Array (ms)", "Linked-list (ms)"]
+    sample_to_times.each do |size, (array, linked_list)|
+      sheet.add_row [size, array, linked_list]
+    end
+
+    sheet.add_chart(Axlsx::ScatterChart, :title => "Arrays vs Linked-list", :scatter_style => :smooth) do |chart|
+      chart.start_at 3, 1
+      chart.end_at 14, 25
+      chart.add_series :xData => sheet["A2:A#{1+size}"], :yData => sheet["B2:B#{1+size}"], :title => "Array", :color => "FF0000"
+      chart.add_series :xData => sheet["A2:A#{1+size}"], :yData => sheet["C2:C#{1+size}"], :title => "Linked-list", :color => "00FF00"
+      
+      puts chart.scatter_style
+    end
+  end
+
+  p.serialize("report.xlsx")
+end
