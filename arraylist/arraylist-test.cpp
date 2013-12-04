32c09f829552c1478711b3cbbf008c2d53603591
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-12-04 14:10:20 +0530

    Add spaces between setup and assertions

--- arraylist/arraylist-test.cpp
+++ arraylist/arraylist-test.cpp
@@ -17,12 +17,14 @@ BOOST_FIXTURE_TEST_SUITE(ArraylistTest, ArraylistTestFixture)
 BOOST_AUTO_TEST_CASE(array_list_is_empty_on_creation)
 {
 	Arraylist list(3);
+
 	BOOST_CHECK_EQUAL(true, list.isEmpty());
 }
 
 BOOST_AUTO_TEST_CASE(array_list_has_reports_items_as_zero_before_insertion)
 {
 	Arraylist list(3);
+
 	BOOST_CHECK_EQUAL(0, list[0]);
 	BOOST_CHECK_EQUAL(0, list[1]);
 }
@@ -31,6 +33,7 @@ BOOST_AUTO_TEST_CASE(array_list_has_items_when_first_item_is_inserted)
 {
 	Arraylist list(3);
 	list.insert(10, 0);
+
 	BOOST_CHECK_EQUAL(false, list.isEmpty());
 	BOOST_CHECK_EQUAL(10, list[0]);
 }
@@ -40,6 +43,7 @@ BOOST_AUTO_TEST_CASE(array_list_has_items_when_second_item_is_inserted_at_end)
 	Arraylist list(3);
 	list.insert(10, 0);
 	list.insert(20, 1);
+
 	BOOST_CHECK_EQUAL(false, list.isEmpty());
 	BOOST_CHECK_EQUAL(10, list[0]);
 	BOOST_CHECK_EQUAL(20, list[1]);
@@ -50,6 +54,7 @@ BOOST_AUTO_TEST_CASE(array_list_has_items_when_second_item_is_inserted_at_start)
 	Arraylist list(3);
 	list.insert(10, 0);
 	list.insert(20, 0);
+
 	BOOST_CHECK_EQUAL(false, list.isEmpty());
 	BOOST_CHECK_EQUAL(20, list[0]);
 	BOOST_CHECK_EQUAL(10, list[1]);
@@ -61,6 +66,7 @@ BOOST_AUTO_TEST_CASE(array_list_has_items_when_third_item_is_inserted_in_middle)
 	list.insert(10, 0);
 	list.insert(20, 1);
 	list.insert(30, 1);
+
 	BOOST_CHECK_EQUAL(false, list.isEmpty());
 	BOOST_CHECK_EQUAL(10, list[0]);
 	BOOST_CHECK_EQUAL(30, list[1]);
