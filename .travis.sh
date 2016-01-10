0d95bacb1853429dc130bdc16e42ad5466f48c75
Chirantan Mitra <chirantan.mitra@gmail.com>
2016-01-10 12:02:29 +0530

    Remove golang specific code

--- .travis.sh
+++ .travis.sh
@@ -8,33 +8,14 @@ cpp_prereqs() {
   sudo apt-get install libboost-test-dev lcov
 }
 
-golang_prereqs() {
-  sudo apt-get update
-  sudo apt-get instal golang
-  go get github.com/mattn/gom
-}
-
 cpp_build() {
   cd linkedlist && make clean coverage && cd ..
   cd arraylist && make clean coverage && cd ..
 }
 
-golang_build() {
-  cd arraylist && gom test -coverprofile=coverage.out && gom tool cover -html=coverage.out -o coverage.html && cd ..
-  cd linkedlist && gom test -coverprofile=coverage.out && gom tool cover -html=coverage.out -o coverage.html && cd ..
-  gom install github.com/chiku/linkedlists-vs-arrays/compare
-  gom install github.com/chiku/linkedlists-vs-arrays/create_report
-}
-
 main() {
-  if [[ "$TRAVIS_BRANCH" == 'cpp' ]]
-  then
-    cpp_prereqs
-    cpp_build
-  else
-    golang_prereqs
-    golang_build
-  fi
+  cpp_prereqs
+  cpp_build
 }
 
 main
